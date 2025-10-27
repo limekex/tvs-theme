1c11c82155d37b8f9b5eab5c4a4d3121e400a1c0
Bjorn-Tore Almas <bjorn-tore@hosalmaas.no>
2025-10-28 00:17:22 +0100

    WIP snapshot: local changes before updating parent (inc/rest.php)

--- inc/rest.php
+++ inc/rest.php
@@ -2,10 +2,15 @@
 /** Development REST endpoints for theme/plugin integration. For production, move to plugin. */
 
 add_action( 'rest_api_init', function () {
+    // If the real plugin is active (TVS_REST class exists), don't register theme mock endpoints
+    if ( class_exists( 'TVS_REST' ) ) {
+        return;
+    }
+
     register_rest_route( 'tvs/v1', '/activities/me', array(
         'methods' => 'GET',
         'callback' => function( $request ) {
-            // Mocked response for development
+            // Mocked response for development (only used when plugin is inactive)
             return rest_ensure_response( array(
                 'activities' => array(),
                 'paging' => array( 'page' => 1 )
